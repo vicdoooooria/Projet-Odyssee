54df68be7d1a7e1aff0a914ccd71b276a778a0fc
TORI <vvespinozaw@hotmail.com>
2026-03-06 14:33:45 +0100

    SOSOSOSOSO

--- Assets/Scripts/FireArrow.cs
+++ Assets/Scripts/FireArrow.cs
@@ -2,33 +2,27 @@ using UnityEngine;
 
 public class FireArrow : MonoBehaviour
 {
-
     public float speed;
     private AudioSource audioSource;
 
-<<<<<<< HEAD
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         audioSource.Play();
     }
-    // Update is called once per frame
-=======
->>>>>>> 0d08fe3fae5ef3af3e6b541041b1830c43e76592
+
     void Update()
     {
-        this.transform.position += Vector3.forward * speed * Time.deltaTime;
+        transform.position += transform.forward * speed * Time.deltaTime;
     }
+
     private void OnCollisionEnter(Collision collision)
     {
-        print(collision.gameObject.name);
-
-        if (collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.CompareTag("Enemy"))
         {
             Destroy(collision.gameObject);
-            Destroy(this.gameObject);
         }
+
+        Destroy(gameObject);
     }
-    
 }
-// rajouter un nombre maximal de tirs par seconde
